8de05d329f91f6847d8a1603901bdc7d92fbe868
GDeLaurentis <g.dl@hotmail.it>
2019-05-23 15:43:14 +0100

    Added __contain__ function for Nums, Dens, Terms, Terms(es). Worked on tests for partial fractioning.

--- test_particle.py
+++ test_particle.py
@@ -1,5 +1,5 @@
 import numpy
-from core.tools import MinkowskiMetric, Pauli_bar
+from antares.core.tools import MinkowskiMetric, Pauli_bar
 from particle import Particle
 
 
@@ -7,6 +7,7 @@ def test_rank_two_spinor_setter():
     oParticle = Particle()
     temp_four_mom = oParticle.four_mom
     p_lowered_index = numpy.dot(MinkowskiMetric, temp_four_mom)
+    # Contraction of 4 momentum with Van der Waerden symbol == outproduct of rank 1 spinors
     r2_s_b_1 = numpy.tensordot(p_lowered_index, Pauli_bar, axes=(0, 0))
     r2_s_b_2 = numpy.tensordot(oParticle.r_sp_d, oParticle.l_sp_d, axes=(1, 0))
     assert(abs(numpy.max(r2_s_b_1 - r2_s_b_2)) < 10 ** -300)
