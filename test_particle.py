e7650f0ebdcb2adaddbf8cb3342f11337d024cbe
GDeLaurentis <g.dl@hotmail.it>
2019-09-20 14:10:32 +0100

    Resolved some dependancies in particles module

--- test_particle.py
+++ test_particle.py
@@ -2,14 +2,9 @@ import numpy
 
 from particle import Particle
 
-from antares.core.tools import MinkowskiMetric, Pauli_bar, flatten
+from antares.core.tools import flatten
 
 
 def test_rank_two_spinor_setter():
     oParticle = Particle()
-    temp_four_mom = oParticle.four_mom
-    p_lowered_index = numpy.dot(MinkowskiMetric, temp_four_mom)
-    # Contraction of 4 momentum with Van der Waerden symbol == outproduct of rank 1 spinors
-    r2_s_b_1 = numpy.tensordot(p_lowered_index, Pauli_bar, axes=(0, 0))
-    r2_s_b_2 = numpy.tensordot(oParticle.r_sp_d, oParticle.l_sp_d, axes=(1, 0))
-    assert(max(map(abs, flatten(r2_s_b_1 - r2_s_b_2))) < 10 ** -300)
+    assert(all([abs(entry1 - entry2) < 10 ** -290 for entry1, entry2 in zip(flatten(oParticle.r2_sp_b), flatten(numpy.tensordot(oParticle.r_sp_d, oParticle.l_sp_d, axes=(1, 0))))]))
