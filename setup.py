3cd49b2fca0c44bd5b39e4959b13ffb200c253be
GDeLaurentis <g.dl@hotmail.it>
2019-09-23 11:27:59 +0100

    Relaxed requirement versions

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
     version='0.0.1',
     author='Giuseppe De Laurentis',
     author_email='g.dl@hotmail.it',
+    url='https://github.com/GDeLaurentis/lips-lite',
     description='Lorentz Invariant Phase Space',
     packages=find_packages(),
-    install_requires=['numpy==1.14.2',
-                      'mpmath==1.0.0',
-                      'sympy==1.1.1', ],
+    install_requires=['numpy<1.17',
+                      'mpmath<=1.1.0',
+                      'sympy<=1.4', ],
 )
