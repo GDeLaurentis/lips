0cbe2924b2c2cd9df17ed068c05f84d5ac7a05c7
GDeLaurentis <g.dl@hotmail.it>
2019-10-02 12:19:38 +0100

    Named v0.1.0

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -25,7 +25,7 @@ author = u'Giuseppe De Laurentis'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.1.0'
+release = u'v0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
