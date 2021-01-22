d884833145059209ab9b6ce620ef5e594056c82c
GDeLaurentis <g.dl@hotmail.it>
2021-01-22 17:19:56 +0100

    Moved updated version of ideal and variety generator into package

--- tests/test_lips_ideal.py
+++ tests/test_lips_ideal.py
@@ -6,7 +6,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 
-from lips import Particles
 from lips.algebraic_geometry.ideal import LipsIdeal
 
 
@@ -14,6 +13,6 @@ from lips.algebraic_geometry.ideal import LipsIdeal
 
 
 def test_LipsIdeal_membership():
-    assert "⟨1|3⟩" in LipsIdeal(Particles(5), ("⟨1|3⟩", "⟨3|(1+5)|3]", ))
-    assert "⟨2|4⟩" not in LipsIdeal(Particles(5), ("⟨1|3⟩", "⟨3|(1+5)|3]", ))
-    assert "⟨3|(1+5)|3]" in LipsIdeal(Particles(5), ("⟨1|3⟩", "⟨3|5⟩", ))
+    assert "⟨1|3⟩" in LipsIdeal(5, ("⟨1|3⟩", "⟨3|(1+5)|3]", ))
+    assert "⟨2|4⟩" not in LipsIdeal(5, ("⟨1|3⟩", "⟨3|(1+5)|3]", ))
+    assert "⟨3|(1+5)|3]" in LipsIdeal(5, ("⟨1|3⟩", "⟨3|5⟩", ))
