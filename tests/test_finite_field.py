238095fa81b27c8fc23a34e3b5e53a3f49d6ab88
GDeLaurentis <g.dl@hotmail.it>
2021-01-28 01:52:09 +0100

    Some tests

--- tests/test_finite_field.py
+++ tests/test_finite_field.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 
 import pickle
 import random
+import pytest
 
 from lips.fields.finite_field import ModP, extended_euclideal_algorithm
 
@@ -15,10 +16,52 @@ from lips.fields.finite_field import ModP, extended_euclideal_algorithm
 
 
 def test_picklable():
-    obj = ModP('2 % 10009')
+    obj = ModP('2 % 10007')
     mydump = pickle.dumps(obj, protocol=2)
     loaded = pickle.loads(mydump)
     assert obj == loaded
+
+
+def test_addition():
+    p = 10007
+    a, b = random.randrange(0, 10000), random.randrange(0, 10000)
+    assert (a + b) % p == ModP(a, p) + ModP(b, p)
+    assert (a + b) % p == a + ModP(b, p)
+    assert (a + b) % p == ModP(a, p) + b
+
+
+def test_subtraction():
+    p = 10007
+    a, b = random.randrange(0, 10000), random.randrange(0, 10000)
+    assert (a - b) % p == ModP(a, p) - ModP(b, p)
+    assert (a - b) % p == a - ModP(b, p)
+    assert (a - b) % p == ModP(a, p) - b
+
+
+def test_multiplication():
+    p = 10007
+    a, b = random.randrange(0, 10000), random.randrange(0, 10000)
+    assert (a * b) % p == ModP(a, p) * ModP(b, p)
+    assert (a * b) % p == a * ModP(b, p)
+    assert (a * b) % p == ModP(a, p) * b
+
+
+def test_negation():
+    p = 10007
+    a = random.randrange(0, 10000)
+    assert (-a) % p == -ModP(a, p)
+
+
+def test_inverse():
+    p = 10007
+    a = ModP(random.randrange(1, p), p)
+    assert a * a._inv() == 1
+
+
+def test_failed_inverse():
+    a = ModP('2 % 4')
+    with pytest.raises(ZeroDivisionError):
+        a * a._inv()
 
 
 def test_extended_euclideal_algorithm():
