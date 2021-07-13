08fdd571cf06e6ebd1094174544ae8311e4ae4f0
GDeLaurentis <g.dl@hotmail.it>
2021-07-13 20:26:14 +0200

    Maximal Quotient Rational Reconstruction + tests + fix to algorithm for (-p, p) case returning false

--- tests/test_finite_field.py
+++ tests/test_finite_field.py
@@ -10,7 +10,7 @@ import random
 import pytest
 
 from fractions import Fraction
-from lips.fields.finite_field import ModP, extended_euclideal_algorithm
+from lips.fields.finite_field import ModP, extended_euclideal_algorithm, rationalise, MQRR, LGRR
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
@@ -84,3 +84,18 @@ def test_extended_euclideal_algorithm():
         a, b = random.randint(1, 1000), random.randint(1, 1000)
         s, t, gcd = extended_euclideal_algorithm(a, b)
         assert(a * s + b * t == gcd)
+
+
+def test_reconstruction_MQRR_2147483647():
+    assert rationalise(298260199, 2147483647, algorithm=MQRR) == Fraction(-51071, 36)
+
+
+def test_reconstruction_LGRR_2147483647():
+    assert rationalise(298260199, 2147483647, algorithm=LGRR) == Fraction(11326, 42041)
+
+
+def test_reconstruction_LGRR_2147483647_pow12():
+    assert rationalise(4479489461410435237106627746985045825552416200368757674235908629372482248377113245126581341636163272702,
+                       9619630365287747226839050681966839463919428531629782475127367001763589500187642982976435876178980851951693987841, algorithm=MQRR) == -1
+    assert rationalise(4479489461410435237106627746985045825552416200368757674235908629372482248377113245126581341636163272702,
+                       9619630365287747226839050681966839463919428531629782475127367001763589500187642982976435876178980851951693987841, algorithm=LGRR) == -1
