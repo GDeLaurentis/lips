e2749b352fc5f838751d1f355f6d66959a1cc901
Giuseppe De Laurentis <g.dl@hotmail.it>
2022-07-30 21:50:27 +0200

    More stringent test for variety generation: invariants must be small, but also momentum conservation and on-shellness must be respected. Hacky fix for weird behaviour of sympy nroot.

--- tests/test_particles_variety.py
+++ tests/test_particles_variety.py
@@ -17,6 +17,7 @@ def test_5_point_codim1_variety_padic():
     oParticles = Particles(5, field=padic)
     oParticles._singular_variety(("[1|2]", ), (1, ), verbose=True)
     assert abs(oParticles("[1|2]")).n == 1
+    assert numpy.all(oParticles.phasespace_consistency_check()[:2])
 
 
 @pytest.mark.skipif(not singular_found, reason="singular not found")
@@ -25,6 +26,7 @@ def test_5_point_codim1_variety_mpc():
     oParticles = Particles(5, field=mpc)
     oParticles._singular_variety(("[1|2]", ), (10 ** -30, ), verbose=True)
     assert numpy.isclose(complex(oParticles("[1|2]")), 10 ** -30)
+    assert numpy.all(oParticles.phasespace_consistency_check()[:2])
 
 
 @pytest.mark.skipif(not singular_found, reason="singular not found")
@@ -34,6 +36,17 @@ def test_5_point_codim2_variety_padic():
     oParticles._singular_variety(("⟨2|1+5|2]", "⟨3|1+2|3]", ), (1, 2, ), verbose=True)
     assert abs(oParticles("⟨2|1+5|2]")).n == 1
     assert abs(oParticles("⟨3|1+2|3]")).n == 2
+    assert numpy.all(oParticles.phasespace_consistency_check()[:2])
+
+
+@pytest.mark.skipif(not singular_found, reason="singular not found")
+def test_5_point_codim2_variety_padic_i_in_field():
+    padic = Field('padic', 2 ** 31 - 19, 4)
+    oParticles = Particles(5, field=padic)
+    oParticles._singular_variety(("⟨2|1+5|2]", "⟨3|1+2|3]", ), (1, 2, ), verbose=True)
+    assert abs(oParticles("⟨2|1+5|2]")).n == 1
+    assert abs(oParticles("⟨3|1+2|3]")).n == 2
+    assert numpy.all(oParticles.phasespace_consistency_check()[:2])
 
 
 @pytest.mark.skipif(not singular_found, reason="singular not found")
@@ -43,3 +56,4 @@ def test_5_point_codim2_variety_mpc():
     oParticles._singular_variety(("⟨2|1+5|2]", "⟨3|1+2|3]", ), (10 ** -30, 2 * 10 ** -30, ), verbose=True)
     assert numpy.isclose(complex(oParticles("⟨2|1+5|2]")), 10 ** -30)
     assert numpy.isclose(complex(oParticles("⟨3|1+2|3]")), 2 * 10 ** -30)
+    assert numpy.all(oParticles.phasespace_consistency_check()[:2])
